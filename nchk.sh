b897f119377f6c3e6e4615a9bff6cd59579f7bcf
without-eyes <dimka180205@gmail.com>
2025-03-29 21:20:15 +0200

    Add checking DNS resolution using "dig" command and "resolv.conf" file

--- nchk.sh
+++ nchk.sh
@@ -1,4 +1,28 @@
-# DNS resolution failure
+#!/bin/bash
+
+# DNS resolution
+url="google.com"
+echo -n "Checking DNS resolution: "
+
+if dig +short "$url" | grep -qE '^[0-9]'; then
+  echo "Success"
+else
+  echo "Failure"
+
+  resolv_file_path="/etc/resolv.conf"
+  echo -n "Problem: "
+
+  if [ ! -f "$resolv_file_path" ]; then
+    echo "$resolv_file_path not found."
+  elif [ ! -s "$resolv_file_path" ]; then
+    echo "$resolv_file_path is empty."
+  else
+    echo "DNS server might be unreachable or misconfigured."
+  fi
+
+  exit 1
+fi
+
 # Packet loss or high latency
 # Wrong IP address, subnet mask, or gateway
 # Routing table issues or misconfigured routes
