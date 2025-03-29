d712b963ba135329078b026ae56eb185426180fa
without-eyes <dimka180205@gmail.com>
2025-03-29 20:25:53 +0200

    Add steps to fix problem as comments

---
+++ nchk.sh
@@ -0,0 +1,9 @@
+# DNS resolution failure
+# Packet loss or high latency
+# Wrong IP address, subnet mask, or gateway
+# Routing table issues or misconfigured routes
+# Firewall blocking traffic
+# ISP issues or upstream network failures
+# Incorrect MAC address configuration
+# Wifi is not turned on or ethernet is not connected
+# Other
