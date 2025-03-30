1dd56b8cb132becce7df6f3605110f11d707cf91
without-eyes <dimka180205@gmail.com>
2025-03-30 16:31:03 +0300

    Add checking if IP address and subnet mask is correctly assigned using "ip" command

--- nchk.sh
+++ nchk.sh
@@ -44,7 +44,22 @@ else
   echo "High latency ($avg_latency ms)"
 fi
 
-# Wrong IP address, subnet mask, or gateway
+# Wrong IP address
+echo -n "Checking for assigned IP address and subnet mask: "
+ip_address="$(ip a | grep "inet " | awk '{print $2}' | tr "/" " " | awk '{print $1}')"
+subnet_mask="$(ip a | grep "inet " | awk '{print $2}' | tr "/" " " | awk '{print $2}')"
+if [ -s ip_address ]; then
+  echo "No IP address"
+  exit 1;
+elif [ -s subnet_mask ]; then
+  echo "No subnet mask"
+  exit 1;
+else
+  echo "Everything is assigned"
+fi
+
+# Bad gateway
+# DHCP is not working(checking link-local addresses)
 # Routing table issues or misconfigured routes
 # Firewall blocking traffic
 # ISP issues or upstream network failures
